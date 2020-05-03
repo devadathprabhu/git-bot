3333641da04aa02528244a82d88308c80209d89a
Devadath Prabhu <dev.kts.31@gmail.com>
2020-05-03 23:31:51 +0530

    Initial commit

---
+++ src/main/java/com/dprabhu/randomcommits/RandomCommitsMain.java
@@ -0,0 +1,13 @@
+/**
+ * @author dprabhu
+ */
+package com.dprabhu.randomcommits;
+
+public class RandomCommitsMain {
+
+  public static void main(String args[]){
+    System.out.println("Git tricks!");
+
+  }
+
+}
